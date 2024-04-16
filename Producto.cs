c64a4494ef385841a4f44c257b1e786155fc4d79
Rodrigorc2803 <rodrigorc2803@gmail.com>
2024-04-15 21:46:44 -0500

    clase Producto

---
+++ Producto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    internal class Producto
+    {
+        public string Nombre { get; set; }
+        public int Tamaño { get; set; }
+        public string Material { get; set; }
+        public string Color { get; set; }
+        public int Precio { get; set; }
+        public float Peso { get; set; }
+
+
+        public void ProductoDeTela(string Nombre, int Tamaño, string Material, string Color, int Precio)
+        {
+            this.Nombre = Nombre;
+            this.Tamaño = Tamaño;
+            this.Material = Material;
+            this.Color = Color;
+            this.Precio = Precio;
+        }
+        public void ProductoDeArcilla(string Nombre, int Peso, string Material, string Color, int Precio)
+        {
+            this.Nombre = Nombre;
+            this.Peso = Peso;
+            this.Material = Material;
+            this.Color = Color;
+            this.Precio = Precio;
+        }
+
+    }
+}
